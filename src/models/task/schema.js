b00f2175224be172707aa3ee4ecba2fc396ce6e3
Bomavi <max.bozh.n1mbo@gmail.com>
2019-07-12 16:38:42 +0300

    Add timestamps to Task schema and createdBy ref

--- src/models/task/schema.js
+++ src/models/task/schema.js
@@ -2,24 +2,26 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const ObjectId = Schema.Types.ObjectId;
 
-const schema = new Schema({
-	description: {
-		type: String,
-		required: true,
+const schema = new Schema(
+	{
+		description: {
+			type: String,
+			required: true,
+		},
+		completed: {
+			type: Boolean,
+			required: true,
+		},
+		createdBy: {
+			type: ObjectId,
+			required: true,
+			ref: 'User',
+		},
 	},
-	completed: {
-		type: Boolean,
-		required: true,
-	},
-	createdBy: {
-		type: ObjectId,
-		required: true,
-	},
-	createdAt: {
-		type: Date,
-		required: true,
-	},
-});
+	{
+		timestamps: true,
+	}
+);
 
 schema.index({
 	description: 'searchable',
