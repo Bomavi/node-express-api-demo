1df25a0a0c216efc15ad061e43e978dc4db7c1a2
Bomavi <max.bozh.n1mbo@gmail.com>
2019-07-12 21:52:46 +0300

    Fix UsersController typo error

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,14 +2,14 @@ const express = require('express');
 
 const { errorHandler } = require('../middleware');
 const { User, Task } = require('../models');
-const { UsersConstroller, TasksController } = require('../controllers');
+const { UsersController, TasksController } = require('../controllers');
 
 const models = { User, Task };
 
 const api = () => {
 	const router = express();
 
-	router.use('/users', UsersConstroller(models));
+	router.use('/users', UsersController(models));
 	router.use('/tasks', TasksController(models));
 	router.use(errorHandler);
 
