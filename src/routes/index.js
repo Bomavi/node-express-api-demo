3f63d40ff0a269b783c85f7e5d9f2f516d6b0f4e
Bomavi <max.bozh.n1mbo@gmail.com>
2019-07-12 18:37:45 +0300

    Add users router

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,15 @@
 const express = require('express');
 
 const { errorHandler } = require('../middleware');
-const { Task } = require('../models');
-const { TasksController } = require('../controllers');
+const { User, Task } = require('../models');
+const { UsersConstroller, TasksController } = require('../controllers');
 
-const models = { Task };
+const models = { User, Task };
 
 const api = () => {
 	const router = express();
 
+	router.use('/users', UsersConstroller(models));
 	router.use('/tasks', TasksController(models));
 	router.use(errorHandler);
 
