697f3e2391ab9c4c37b91ec412b1f259b0b92c4a
Bomavi <max.bozh.n1mbo@gmail.com>
2019-07-22 01:37:57 +0300

    Change mongo config

--- src/config/mongo/index.js
+++ src/config/mongo/index.js
@@ -1,14 +1,10 @@
 const { connect, connection } = require('mongoose');
-const dotenv = require('dotenv');
 
 const { debugLogger } = rootRequire('utils');
 
-dotenv.config();
-
-const MONGO_DB_HOST =
-	process.env.NODE_ENV === 'production' ? process.env.MONGO_DB_HOST : 'mongodb://localhost';
-const MONGO_DB_PORT = process.env.MONGO_DB_PORT;
-const DB_NAME = process.env.DB_NAME;
+const MONGO_DB_HOST = process.env.MONGO_DB_HOST || process.env.MONGO_DB_DEV_HOST;
+const MONGO_DB_PORT = process.env.MONGO_DB_PORT || process.env.MONGO_DB_DEV_PORT;
+const DB_NAME = process.env.DB_NAME || process.env.DB_DEV_NAME;
 const connectionUrl = `${MONGO_DB_HOST}:${MONGO_DB_PORT}/${DB_NAME}`;
 const options = {
 	useNewUrlParser: true,
