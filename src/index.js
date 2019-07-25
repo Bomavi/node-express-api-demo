ea9e0df5d6ddd2a6097f1b8422cd21c34919421b
Bomavi <max.bozh.n1mbo@gmail.com>
2019-07-25 21:06:37 +0300

    Cleanup, add configs and fix names

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
-// const cors = require('cors');
 
 /* Declare global rootRequire for "absolute" paths */
 global.rootRequire = name => require(`${__dirname}/${name}`);
@@ -9,6 +8,7 @@ global.rootRequire = name => require(`${__dirname}/${name}`);
 const { debugLogger } = rootRequire('utils');
 const { api } = rootRequire('routes');
 const {
+	service,
 	mongoConnect,
 	redis: { session, redisOptions, redisStore },
 } = rootRequire('config');
@@ -16,41 +16,43 @@ const {
 /* Get .env constants */
 dotenv.config();
 
-const isProd = process.env.NODE_ENV === 'production';
-const PORT = isProd ? process.env.PORT : process.env.DEV_PORT;
-const maxAge = process.env.SESSION_EXPIRES_IN * 1000 || 3600 * 1000;
+/* initialize microservice with credentials */
+service.init();
+
+// const isProd = process.env.NODE_ENV === 'production';
+const PORT = process.env.PORT || process.env.DEV_PORT;
+const maxAge = Number(process.env.SESSION_EXPIRES_IN) * 1000;
 const app = express();
 
+/* Initialize middlewares and express-session with REDIS */
+app.set('trust proxy', true);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-/* Use CORS in non production env */
-// if (!isProd) {
-// 	app.options('*', cors({ credentials: true }));
-// 	app.use(cors({ credentials: true }));
-// }
-
-/* Initialize MONGO configuration */
-mongoConnect();
-
-/* Initialize express-session with REDIS */
-app.set('trust proxy', 1);
 app.use(
 	session({
 		store: new redisStore(redisOptions),
 		secret: process.env.SESSION_SECRET,
 		resave: true,
-		rolling: true,
+		rolling: false,
 		saveUninitialized: false,
+		// proxy: true,
 		cookie: {
+			path: '/',
+			domain: 'localhost',
 			maxAge,
-			secure: isProd,
+			httpOnly: true,
+			sameSite: true,
+			// secure: isProd,
+			secure: false,
 		},
 	})
 );
 
+/* Initialize MONGO configuration */
+mongoConnect();
+
 /* Initialize app routes */
-app.use('/api', api());
+app.use(`/services/${process.env.SERVICE_NAME}`, api());
 
 app.listen(PORT, () => {
 	debugLogger('app', `Server is running on port ${PORT}`);
