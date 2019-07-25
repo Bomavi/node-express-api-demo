505f4519120a43c0ebb13869f2694f137cf37544
Bomavi <max.bozh.n1mbo@gmail.com>
2019-07-25 21:55:57 +0300

    Cleanup and modify ValidateController

--- src/controllers/validate/index.js
+++ src/controllers/validate/index.js
@@ -5,30 +5,40 @@ const mongoose = require('mongoose');
 const ObjectId = mongoose.Types.ObjectId;
 
 const validateUser = ({ User }) => async (req, res, next) => {
-	const { username, password } = req.body;
+	const { username, password, register } = req.body;
+
+	if (!username) {
+		return next(createError(404, `username is a required parameter!`));
+	}
+
 	const user = await User.findOne({ username });
 
 	if (user && user.password !== password) {
+		if (register) {
+			return next(createError(401, `user "${username}" already exists!`));
+		}
 		return next(createError(401, `credentials for "${username}" invalid!`));
 	}
 
-	if (!user && username === 'guest') {
+	if (!user && (username === 'guest' || register)) {
 		try {
 			const newUser = await User.create({
 				_id: ObjectId(),
-				firstname: 'John',
-				lastname: 'Doe',
 				username,
-				password: 'parol',
+				password,
 			});
 
-			return res.status(200).send({ userId: newUser._id });
+			return res.status(200).send(newUser);
 		} catch (e) {
 			return next(e);
 		}
 	}
 
-	res.status(200).send({ userId: user._id });
+	if (!user) {
+		return next(createError(404, `user "${username}" not found or credentials invalid!`));
+	}
+
+	res.status(200).send(user);
 };
 
 module.exports = models => {
