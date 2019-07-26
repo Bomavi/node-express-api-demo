fafc64341bb07d6db32eb1100a108083508092b9
Bomavi <max.bozh.n1mbo@gmail.com>
2019-07-26 15:52:48 +0300

    Cleanup

--- src/models/User/schema.js
+++ src/models/User/schema.js
@@ -4,14 +4,6 @@ const ObjectId = Schema.Types.ObjectId;
 
 const schema = new Schema(
 	{
-		// firstname: {
-		// 	type: String,
-		// 	required: true,
-		// },
-		// lastname: {
-		// 	type: String,
-		// 	required: true,
-		// },
 		username: {
 			type: String,
 			required: true,
