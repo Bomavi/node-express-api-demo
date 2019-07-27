e656f25f35d5565418f0b60a6be32926d0cbbb30
Bomavi <max.bozh.n1mbo@gmail.com>
2019-07-27 23:34:34 +0300

    Modify mongoose PRE method

--- src/models/Task/index.js
+++ src/models/Task/index.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 const { schema } = require('./schema');
 
-// schema.pre('save', () => {
-// 	return doStuff();
+// schema.pre('save', function(next) {
+// 	next();
 // });
 
 module.exports = mongoose.model('Task', schema);
